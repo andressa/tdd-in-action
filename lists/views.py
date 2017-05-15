7b9e826f2b6a9669d394b3eb6c093f27ef1a31af
Andressa Sivolella <andressa.sivolella@twistsystems.com>
2017-05-15 18:11:40 -0300

    Add list object and item

--- lists/views.py
+++ lists/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
-    if request.method == 'POST':
-        new_item = request.POST['item_text']
-        Item.objects.create(text=new_item)
-        return redirect('/')
+    return render(request, 'home.html')
 
-    items = Item.objects.all()
+
+def view_list(request, list_id):
+    list = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list)
     return render(
         request,
-        'home.html',
-        {'items': items}
+        'list.html',
+        {'list': list}
     )
+
+
+def new_list(request):
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
+
+
+def add_item(request, list_id):
+    list = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list)
+    return redirect(f'/lists/{list.id}/')
