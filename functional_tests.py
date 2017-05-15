fdda4d650c008f434b665b2a07f6558f1d1fbaf4
Andressa Sivolella <andressa.sivolella@twistsystems.com>
2017-05-15 12:17:12 -0300

    First url mapping and unit test

--- functional_tests.py
+++ functional_tests.py
@@ -1,8 +1,22 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome()
 
-browser.close()
+    def tearDown(self):
+        self.browser.close()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('To-do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
